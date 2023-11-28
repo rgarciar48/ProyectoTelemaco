50e072a18d50d2dd720d4ece36c9b1246aef92b0
rgarciar48 <rgarciar48@educarex.es>
2023-11-28 09:14:39 +0100

    Segundo incio

--- src/MisClases/HolaMundo.java
+++ src/MisClases/HolaMundo.java
@@ -5,6 +5,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hola Mundo");
+		System.out.println("Buenoos dias");
+		
 	}
 
 }
